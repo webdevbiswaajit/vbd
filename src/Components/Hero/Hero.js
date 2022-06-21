c8c7d41aaef33b5f8d2a05d245f0fa9320cdbb9b
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-06-21 16:24:47 +0600

    Vedio add

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,9 +1,34 @@
 import React from "react";
 import { BsFillPlayCircleFill } from "react-icons/bs";
-import { Container } from "react-bootstrap";
+import { Container, Modal } from "react-bootstrap";
+import ReactPlayer from "react-player";
+import vedio from "./vedio/BusinessService .mp4";
 import "./Hero.css";
 
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <ReactPlayer
+        url={vedio}
+        playing={true}
+        muted={true}
+        controls={true}
+        volume={1}
+        width="80wh"
+        height="60vh"
+        onReady={() => console.log("ready now")}
+      />
+    </Modal>
+  );
+}
+
 function Hero() {
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <>
       <section className="hero">
@@ -15,8 +40,16 @@ function Hero() {
               Tommorrow Business
             </h1>
           </div>
+
+          <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
           <div className="play">
-            <BsFillPlayCircleFill className="play_btn heartbeat " />
+            <BsFillPlayCircleFill
+              onClick={() => setModalShow(true)}
+              className="play_btn heartbeat "
+            />
             <p>How we work</p>
           </div>
         </Container>
