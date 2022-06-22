77b97e13ac177b86cb48b692771a2d1d13162b34
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-06-22 15:33:51 +0600

    Menu issue fixed

--- src/Components/Career/Apply/Apply.js
+++ src/Components/Career/Apply/Apply.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import apply from "./ApplyData";
 import "./Apply.css";
+import { ExternalLink } from "react-external-link";
 
 function Apply() {
   return (
@@ -17,104 +19,37 @@ function Apply() {
             </h2>
           </div>
 
-          <div className="get_job">
-            <Row>
-              <Col md={3} sm={12}>
-                <div className="job_expertis">
-                  <small>React/ Laravel</small>
-                </div>
-                <div className="job_name">
-                  <h2>
-                    Web Developer <span>/ Full Time</span>
-                  </h2>
-                </div>
-                <div className="selary">
-                  <p>Expected Salary - 10k - 15k</p>
-                </div>
-              </Col>
-              <Col md={6} sm={12}>
-                <div className="job_text">
-                  <p>
-                    We are currently seeking a highly motivated individual to
-                    fill an I.T. This position will provide remote and onsite
-                    hands-on support to staff, vendors, and customers as well
-                    as, proactively identify issues.
-                  </p>
-                </div>
-              </Col>
-              <Col md={3} sm={12}>
-                <div className="job_apply">
-                  <Link to="/">Apply</Link>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          {/* ============================= */}
-          <div className="get_job">
-            <Row>
-              <Col md={3} sm={12}>
-                <div className="job_expertis">
-                  <small>Photoshop/ Illustrator</small>
-                </div>
-                <div className="job_name">
-                  <h2>
-                    Graphic Designer<span>/ Full Time</span>
-                  </h2>
-                </div>
-                <div className="selary">
-                  <p>Expected Salary - 10k - 15k</p>
-                </div>
-              </Col>
-              <Col md={6} sm={12}>
-                <div className="job_text">
-                  <p>
-                    We are currently seeking a highly motivated individual to
-                    fill an I.T. This position will provide remote and onsite
-                    hands-on support to staff, vendors, and customers as well
-                    as, proactively identify issues.
-                  </p>
-                </div>
-              </Col>
-              <Col md={3} sm={12}>
-                <div className="job_apply">
-                  <Link to="/">Apply</Link>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          {/* ========================== */}
-          <div className="get_job">
-            <Row>
-              <Col md={3} sm={12}>
-                <div className="job_expertis">
-                  <small>Marketing</small>
-                </div>
-                <div className="job_name">
-                  <h2>
-                    Digital Marketer <span>/ Full Time</span>
-                  </h2>
-                </div>
-                <div className="selary">
-                  <p>Expected Salary - 10k - 15k</p>
-                </div>
-              </Col>
-              <Col md={6} sm={12}>
-                <div className="job_text">
-                  <p>
-                    We are currently seeking a highly motivated individual to
-                    fill an I.T. This position will provide remote and onsite
-                    hands-on support to staff, vendors, and customers as well
-                    as, proactively identify issues.
-                  </p>
-                </div>
-              </Col>
-              <Col md={3} sm={12}>
-                <div className="job_apply">
-                  <Link to="/">Apply</Link>
-                </div>
-              </Col>
-            </Row>
-          </div>
+          {apply.map((data) => (
+            <div className="get_job">
+              <Row>
+                <Col md={3} sm={12}>
+                  <div className="job_expertis">
+                    <small>{data.job_expertis}</small>
+                  </div>
+                  <div className="job_name">
+                    <h2>
+                      {data.job_name} <span>/ {data.jobNature}</span>
+                    </h2>
+                  </div>
+                  <div className="selary">
+                    <p>Expected Salary - {data.Salary}</p>
+                  </div>
+                </Col>
+                <Col md={6} sm={12}>
+                  <div className="job_text">
+                    <p>{data.jobDes}</p>
+                  </div>
+                </Col>
+                <Col md={3} sm={12}>
+                  <div className="job_apply">
+                    <ExternalLink href="https://www.gmail.com">
+                      Apply
+                    </ExternalLink>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          ))}
         </Container>
       </section>
     </>
---
+++ src/Components/Career/Apply/ApplyData.js
@@ -0,0 +1,35 @@
+const apply = [
+  {
+    job_expertis: "React/ Laravel",
+    job_name: "Web Developer",
+    jobNature: "Full Time",
+    Salary: "10k-15k",
+    jobDes:
+      " We are currently seeking a highly motivated individual to fill an I.T. This position will provide remote and onsite hands-on support to staff, vendors, and customers as well as, proactively identify issues.",
+  },
+  {
+    job_expertis: " Laravel",
+    job_name: "Web Developer",
+    jobNature: "Intern",
+    Salary: "5k-7k",
+    jobDes:
+      " We are currently seeking a highly motivated individual to fill an I.T. This position will provide remote and onsite hands-on support to staff, vendors, and customers as well as, proactively identify issues.",
+  },
+  {
+    job_expertis: "React/ Laravel",
+    job_name: "Web Developer",
+    jobNature: "Full Time",
+    Salary: "10k-15k",
+    jobDes:
+      " We are currently seeking a highly motivated individual to fill an I.T. This position will provide remote and onsite hands-on support to staff, vendors, and customers as well as, proactively identify issues.",
+  },
+  {
+    job_expertis: "React/ Laravel",
+    job_name: "Web Developer",
+    jobNature: "Full Time",
+    Salary: "10k-15k",
+    jobDes:
+      " We are currently seeking a highly motivated individual to fill an I.T. This position will provide remote and onsite hands-on support to staff, vendors, and customers as well as, proactively identify issues.",
+  },
+];
+export default apply;
